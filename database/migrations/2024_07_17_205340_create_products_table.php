19a0328a7444e9ef16e4416a4aa808d05e934c61
Nikita Dibrov <dibrovnik@yandex.ru>
2024-07-18 16:12:44 +0300

    create first hello, world response

--- database/migrations/2024_07_17_205340_create_products_table.php
+++ database/migrations/2024_07_17_205340_create_products_table.php
@@ -13,7 +13,6 @@ return new class extends Migration {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->nullOnDelete();
-            // $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
             $table->string('name')->nullable();
             $table->mediumText('description')->nullable();
             $table->integer('count')->nullable();
